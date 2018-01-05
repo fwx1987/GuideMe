285cb3dbc2a8ef1b923996c84a74046dab91b69b
wenxiang <wenxiang.feng@gmail.com>
2018-01-05 17:29:00 +0800

    initial release

---
+++ polls/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.utils import timezone
+from django.shortcuts import  get_object_or_404,render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+import datetime
+# Create your models here.
+# Change your models (in models.py).
+# Run python manage.py makemigrations to create migrations for those changes
+# Run python manage.py migrate to apply those changes to the database.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
